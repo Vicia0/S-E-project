22e9ec6cd353846672b1bdc8abb066456495436e
SylvieMutuyimana <s.mutuyiman@alustudent.com>
2022-10-09 04:05:11 +0300

    driver login

--- webproject/webapp/views.py
+++ webproject/webapp/views.py
@@ -1,17 +1,30 @@
+from this import d
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
 
+#home
 #def index(request,*args,**kwargs):
 def home(request):
     context = {}
     #i.e context['webname'] = "RBIS"
     return render(request, "start-page.html", context )
+
+#passenger
 def passenger_login(request):
     context = {}
-    #i.e context['webname'] = "RBIS"
     return render(request, "login-passenger.html", context )
+
+
+#driver
+def driver_login(request):
+    context = {}
+    return render(request, "login-driver.html", context )
+def driver_homepage(request):
+    context = {}
+    return render(request, "driver-homepage.html", context)
+
+#check
 def check(request):
     return HttpResponse("Hello world!")
 
